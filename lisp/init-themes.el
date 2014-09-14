df6b8110d30e7aff61e97209c26ae3e1ea8aa1f3
zhen.pei <jaypei97159@gmail.com>
2014-09-14 14:19:29 +0800

    Theme support console.

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -1,13 +1,29 @@
+;;; Package --- Themes
+;;; Commentary:
+
+;;; Code:
+
+
 (when (< emacs-major-version 24)
   (require-package 'color-theme))
 
 (require-package 'monokai-theme)
+(require-package 'cyberpunk-theme)
+(require-package 'zenburn-theme)
 
 (defun reapply-themes ()
   "Load theme."
-  (require 'monokai-theme))
+  (require (if (display-graphic-p)
+               'monokai-theme
+             'zenburn-theme)))
 
 (add-hook 'after-init-hook 'reapply-themes)
 
 
 (provide 'init-themes)
+;;; init-themes.el ends here
+
+;; Local Variables:
+;; coding: utf-8
+;; no-byte-compile: t
+;; End:

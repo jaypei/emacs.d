f0ee874268689486b3d802b9a5c6c3589c44bb3f
jaypei <jaypei97159@gmail.com>
2014-09-18 23:18:54 +0800

    Add GOPATH for osx

--- lisp/init-exec-path.el
+++ lisp/init-exec-path.el
@@ -1,7 +1,13 @@
+;;; Package --- Import shell environ variables
+;;; Commentary:
+
+;;; Code:
+
 (require-package 'exec-path-from-shell)
 
 (after-load 'exec-path-from-shell
-  (dolist (var '("SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO" "LANG" "LC_CTYPE"))
+  (dolist (var '("SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO"
+                 "LANG" "LC_CTYPE" "GOPATH" "PIP_DOWNLOAD_CACHE"))
     (add-to-list 'exec-path-from-shell-variables var)))
 
 
@@ -9,3 +15,9 @@
   (exec-path-from-shell-initialize))
 
 (provide 'init-exec-path)
+;;; init-exec-path.el ends here
+
+;; Local Variables:
+;; coding: utf-8
+;; no-byte-compile: t
+;; End:

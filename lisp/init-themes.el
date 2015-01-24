1f04d75eb0232c529f0f476b0cfdf4f8e6f9aed8
Jaypei <jaypei97159@gmail.com>
2015-01-24 22:32:26 +0800

    use ir-black-theme

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -7,14 +7,16 @@
 (when (< emacs-major-version 24)
   (require-package 'color-theme))
 
-(require-package 'monokai-theme)
-(require-package 'cyberpunk-theme)
 (require-package 'zenburn-theme)
+(require-package 'ir-black-theme)
+;; (require-package 'monokai-theme)
+;; (require-package 'cyberpunk-theme)
+;; (require-package 'monochrome-theme)
 
 (defun reapply-themes ()
   "Load theme."
   (require (if (display-graphic-p)
-               'monokai-theme
+               'ir-black-theme
              'zenburn-theme)))
 
 ;; (add-hook 'after-init-hook 'reapply-themes)

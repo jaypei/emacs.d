b7a3ddbb5bf88b5ce01439d601c67a64005ba926
jaypei <jaypei97159@gmail.om>
2015-01-31 13:01:34 +0800

    Arduino support

---
+++ lisp/init-arduino.el
@@ -0,0 +1,15 @@
+;;; Package --- Arduino
+;;; Commentary:
+;;; Code:
+
+
+(require-package 'arduino-mode)
+
+
+(provide 'init-arduino)
+;;; init-arduino.el ends here
+
+;; Local Variables:
+;; coding: utf-8
+;; no-byte-compile: t
+;; End:
